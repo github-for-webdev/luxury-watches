d732aef05e18acb27ae5073d279a95942f892723
Andrey <steamprofile2020@gmail.com>
2022-01-25 20:46:31 +0300

    transfer of layout

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -7,21 +7,7 @@ use ishop\Cache;
 class MainController extends AppController {
 
     public function indexAction() {
-        $posts = \R::findAll('test');
-        $post = \R::findOne('test', 'id = ?', [2]);
         $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
-        $name = 'value_name';
-        $age = 'value_age';
-        $names = ['value_name(1)', 'value_name(2)'];
-        $cache = Cache::instance();
-        // $cache->set('test', $names);
-        // $cache->delete('test');
-        $data = $cache->get('test');
-        if (!$data) {
-            $cache->set('test', $names);
-        }
-        debug($data);
-        $this->set(compact('name', 'age', 'names', 'posts'));
     }
 
 }
