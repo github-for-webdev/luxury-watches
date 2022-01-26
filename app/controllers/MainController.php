dbe9a9c03913c1a46b351235e1187dc6f6dd70ad
Andrey <steamprofile2020@gmail.com>
2022-01-26 20:34:36 +0300

    dynamic homepage

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -7,7 +7,9 @@ use ishop\Cache;
 class MainController extends AppController {
 
     public function indexAction() {
+        $brands = \R::find('brand', 'LIMIT 3');
         $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
+        $this->set(compact('brands'));
     }
 
 }
