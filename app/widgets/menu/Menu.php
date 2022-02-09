db4c567f3c4e09a5c5d25990346d6109eba769e4
Andrey <steamprofile2020@gmail.com>
2022-02-09 22:45:57 +0300

    categories widget

--- app/widgets/menu/Menu.php
+++ app/widgets/menu/Menu.php
@@ -4,14 +4,16 @@ namespace app\widgets\menu;
 
 use ishop\App;
 use ishop\Cache;
+use RedUNIT\Base\Threeway;
 
-class Menu {
+class Menu{
 
     protected $data;
     protected $tree;
     protected $menuHtml;
     protected $tpl;
     protected $container = 'ul';
+    protected $class = 'menu';
     protected $table = 'category';
     protected $cache = 3600;
     protected $cacheKey = 'ishop_menu';
@@ -21,7 +23,6 @@ class Menu {
     public function __construct($options = []) {
         $this->tpl = __DIR__ . '/menu_tpl/menu.php';
         $this->getOptions($options);
-        debug($this->table);
         $this->run();
     }
 
@@ -41,24 +42,53 @@ class Menu {
             if (!$this->data) {
                 $this->data = $cats = \R::getAssoc("SELECT * FROM {$this->table}");
             }
+            $this->tree = $this->getTree();
+            $this->menuHtml = $this->getMenuHtml($this->tree);
+            if ($this->cache) {
+                $cache->set($this->cacheKey, $this->menuHtml, $this->cache);
+            }
         }
         $this->output();
     }
 
     protected function output() {
-        echo $this->menuHtml;
+        $attrs = '';
+        if (!empty($this->attrs)) {
+            foreach($this->attrs as $key => $value) {
+                $attrs .= " $key='$value' ";
+            }
+        }
+        echo "<{$this->container} class='{$this->class}' $attrs>";
+            echo $this->prepend;
+            echo $this->menuHtml;
+        echo "</{$this->container}>";
     }
 
     protected function getTree() {
-
+        $tree = [];
+        $data = $this->data;
+        foreach ($data as $id=>&$node) {
+            if (!$node['parent_id']) {
+                $tree[$id] = &$node;
+            } else {
+                $data[$node['parent_id']]['childs'][$id] = &$node;
+            }
+        }
+        return $tree;
     }
 
     protected function getMenuHtml($tree, $tab = '') {
-
+        $str = '';
+        foreach ($tree as $id => $category) {
+            $str .= $this->catToTemplate($category, $tab, $id);
+        }
+        return $str;
     }
 
     protected function catToTemplate($category, $tab, $id) {
-
+        ob_start();
+        require $this->tpl;
+        return ob_get_clean();
     }
 
 }
