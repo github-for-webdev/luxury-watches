06cf84bfd95e2eb9c3bef5a9e3ecbdfc01e61c4b
Andrey <steamprofile2020@gmail.com>
2022-02-11 22:25:49 +0300

    card product

---
+++ app/controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\controllers;
+
+class ProductController extends AppController {
+
+    public function viewAction() {
+        $alias = $this->route['alias'];
+        $product = \R::findOne('product', "alias = ? AND status = '1'", [$alias]);
+        if (!$product) {
+            throw new \Exception('Страница не найдена', 404);
+        }
+        $this->setMeta($product->title, $product->description, $product->keywords);
+        $this->set(compact('product'));
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -2,7 +2,7 @@
 
 use ishop\Router;
 
-
+Router::add('^product/(?P<alias>[a-z0-9-]+)/?$', ['controller' => 'Product', 'action' => 'view']);
 
 Router::add('^admin$', ['controller' => 'Main', 'action' => 'index', 'prefix' => 'admin']);
 Router::add('^admin/?(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$', ['prefix' => 'admin']);
