6cbcc0f891e378246cc82d2dbcb7b11a7d3e97f0
Andrey <steamprofile2020@gmail.com>
2022-02-14 01:31:21 +0300

    card product, gallery pictures

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -11,8 +11,9 @@ class ProductController extends AppController {
             throw new \Exception('Страница не найдена', 404);
         }
         $related = \R::getAll("SELECT * FROM related_product JOIN product ON product.id = related_product.related_id WHERE related_product.product_id = ?", [$product->id]);
+        $gallery = \R::findAll('gallery', 'product_id = ?', [$product->id]);
         $this->setMeta($product->title, $product->description, $product->keywords);
-        $this->set(compact('product', 'related'));
+        $this->set(compact('product', 'related', 'gallery'));
     }
 
 }
