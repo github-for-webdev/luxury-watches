7923b1d9c2827b40f62b4ad0479ec3fa72674fcd
github-for-webdev <steamprofile2020@gmail.com>
2022-04-14 12:42:58 +0300

    Filters

--- app/widgets/filter/Filter.php
+++ app/widgets/filter/Filter.php
@@ -37,6 +37,10 @@ class Filter
     protected function getHtml()
     {
         ob_start();
+        $filter = self::getFilter();
+        if (!empty($filter)) {
+            $filter = explode(',', $filter);
+        }
         require $this->tpl;
         return ob_get_clean();
     }
