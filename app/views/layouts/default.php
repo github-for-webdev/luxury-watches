ad5e08cc9038661b2efbeb6e8566fc16f2760546
github-for-webdev <steamprofile2020@gmail.com>
2022-04-17 23:36:44 +0300

    Authorization of templates

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -1,16 +1,19 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php echo $this->getMeta(); ?>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?= $this->getMeta(); ?>
 </head>
 
 <body>
+
     <h1>Шаблон DEFAULT</h1>
-    <?php echo $content; ?>
+
+    <?= $content; ?>
+
     <?php
     $logs = \R::getDatabaseAdapter()
         ->getDatabase()
