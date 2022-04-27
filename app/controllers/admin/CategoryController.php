01aa6291009a84e95fc4d5b02024521b57c9b951
github-for-webdev <steamprofile2020@gmail.com>
2022-04-27 21:58:09 +0300

    Category management

--- app/controllers/admin/CategoryController.php
+++ app/controllers/admin/CategoryController.php
@@ -1,52 +1,59 @@
 <?php
 
-namespace app\controllers\admin;
+namespace app\controllers;
 
+use app\models\Breadcrumbs;
 use app\models\Category;
+use app\widgets\filter\Filter;
+use ishop\App;
+use ishop\libs\Pagination;
 
 class CategoryController extends AppController
 {
 
-    public function indexAction()
+    public function viewAction()
     {
-        $this->setMeta('Список категорий');
-    }
+        $alias = $this->route['alias'];
+        $category = \R::findOne('category', 'alias = ?', [$alias]);
+        if (!$category) {
+            throw new \Exception('Страница не найдена', 404);
+        }
 
-    public function deleteAction()
-    {
-        $id = $this->getRequestID();
-        $children = \R::count('category', 'parent_id = ?', [$id]);
-        $errors = '';
-        if ($children) {
-            $errors .= '<li>Удаление невозможно, в категории есть вложенные категории</li>';
-        }
-        $products = \R::count('product', 'category_id = ?', [$id]);
-        if ($products) {
-            $errors .= '<li>Удаление невозможно, в категории есть товары</li>';
-        }
-        if ($errors) {
-            $_SESSION['error'] = "<ul>$errors</ul>";
-            redirect();
-        }
-        $category = \R::load('category', $id);
-        \R::trash($category);
-        $_SESSION['success'] = 'Категория удалена';
-        redirect();
-    }
+        $breadcrumbs = Breadcrumbs::getBreadcrumbs($category->id);
 
-    public function addAction()
-    {
-        if (!empty($_POST)) {
-            $category = new Category();
-            $data = $_POST;
-            $category->load($data);
-            if (!$category->validate($data)) {
-                $category->getErrors();
-                redirect();
-            }
-            if ($id = $category->save('category')) {
+        $cat_model = new Category();
+        $ids = $cat_model->getIds($category->id);
+        $ids = !$ids ? $category->id : $ids . $category->id;
+
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $perpage = App::$app->getProperty('pagination');
+
+        $sql_part = '';
+        if (!empty($_GET['filter'])) {
+            /*
+            SELECT `product`.*  FROM `product`  WHERE category_id IN (6) AND id IN
+            (
+            SELECT product_id FROM attribute_product WHERE attr_id IN (1,5) GROUP BY product_id HAVING COUNT(product_id) = 2
+            )
+            */
+            $filter = Filter::getFilter();
+            if ($filter) {
+                $cnt = Filter::getCountGroups($filter);
+                $sql_part = "AND id IN (SELECT product_id FROM attribute_product WHERE attr_id IN ($filter) GROUP BY product_id HAVING COUNT(product_id) = $cnt)";
             }
         }
-        $this->setMeta('Новая категория');
+
+        $total = \R::count('product', "category_id IN ($ids) $sql_part");
+        $pagination = new Pagination($page, $perpage, $total);
+        $start = $pagination->getStart();
+
+        $products = \R::find('product', "category_id IN ($ids) $sql_part LIMIT $start, $perpage");
+
+        if ($this->isAjax()) {
+            $this->loadView('filter', compact('products', 'total', 'pagination'));
+        }
+
+        $this->setMeta($category->title, $category->description, $category->keywords);
+        $this->set(compact('products', 'breadcrumbs', 'pagination', 'total'));
     }
 }
